cb1f17f17dcafe96976582029c9615a69b0389ba
ravenfury2 <ravenfury2@gmail.com>
2025-09-14 14:54:20 -0400

    Constants file added, imported into main.py, game loop established with conditional statement to exit the loop.

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+# import everything from the module
+# constants.py into the current file
+from constants import *
+
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill("black")
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
