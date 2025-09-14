9e16805836cd129bd2d94ccc05080582046e2793
ravenfury2 <ravenfury2@gmail.com>
2025-09-14 15:18:54 -0400

    Set a FPS limiter for the screen refresh.

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -17,8 +20,12 @@ def main():
                 running = False
         
         screen.fill("black")
-
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        
+        
+        
 
 
 if __name__ == "__main__":
