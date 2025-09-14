af65a1f5ab95ca2cdec106a7bdac416c3b6314ec
ravenfury2 <ravenfury2@gmail.com>
2025-09-14 16:40:59 -0400

    Created groups so it will not clutter the main game loop.

--- main.py
+++ main.py
@@ -9,14 +9,21 @@ from constants import *
 
 # import everything from the module
 # player.py into the current file
-from player import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+       
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     running = True
     while running:
@@ -24,16 +31,18 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
+        updatable.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for objects in drawable:
+            objects.draw(screen)
+        
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
         
         
-        
-
-
 if __name__ == "__main__":
     main()
