16c49397ece725e997048e170e4aba97ab515e15
ravenfury2 <ravenfury2@gmail.com>
2025-09-15 18:31:47 -0400

    Made asteroid lumpy, and retain their randomized shape.

--- asteroid.py
+++ asteroid.py
@@ -1,14 +1,35 @@
 import pygame
 from constants import *
 from circleshape import *
+import math
 import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
+        self.lumpy_points = []
+        num_segments = random.randint(ASTEROID_MIN_NUM_SEGMENTS, ASTEROID_MAX_NUM_SEGMENTS)
+        base_angle_increment = (2 * math.pi) / num_segments
+
+        for i in range(num_segments):
+            angle = i * base_angle_increment
+            
+            perturbed_radius = self.radius * random.uniform(ASTEROID_MIN_LUMP_FACTOR, ASTEROID_MAX_LUMP_FACTOR)
+                        
+            px_offset = perturbed_radius * math.cos(angle)
+            py_offset = perturbed_radius * math.sin(angle)
+
+            self.lumpy_points.append(pygame.Vector2(px_offset, py_offset))
+    
+
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
+        if len(self.lumpy_points) > 2:
+            transformed_points = []
+            for p_offset in self.lumpy_points:
+                transformed_points.append(self.position + p_offset)
+            pygame.draw.polygon(screen, "white", transformed_points, width=2)
+
 
     def update(self, dt):
         self.position += self.velocity * dt
